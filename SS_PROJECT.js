53de80dd3dda75bebee7a258f2fd8c10eb2da98f
Mintdot <nigthmare100@naver.com>
2018-01-20 16:33:00 +0900

    흠

--- SS_PROJECT.js
+++ SS_PROJECT.js
@@ -1,91 +1,3 @@
-// function SScalendar(ssYear, ssMonth) {
-//var today = new Date();
-//console.log(today);
-//var year = today.getFullYear();
-//var month = today.getMonth();
-//var date = today.getDate();
-//var day = today.getDay();
-//var lastDate = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-//    
-//if(ssYear==null) ssYear=year;
-//if(ssMonth==null) ssMonth=month;
-//    
-//ssToday = new Date();
-//ssToday.setFullYear(ssYear);
-//ssToday.setMonth(ssMonth);
-//ssToday.setDate(1);
-//var ssDay = ssToday.getDay();
-//
-//
-//if(ssYear%4==0 && ssYear%100!=0 || ssYear%400==0)
-//    lastDate[1] = 29;
-//
-//var nowLastDate = lastDate[ssMonth];
-//
-//var week = Math.ceil((ssDay+nowLastDate)/7);
-//
-//var prevMonthToday = new Date(year, month-1, date);
-//var nextMonthToday = new Date(year, month+1, date);
-//
-//console.log(prevMonthToday);
-//console.log(nextMonthToday);
-//
-//var calendar = "<div class='calendar_div'>";
-//calendar += "<p>";
-//calendar += "<a href='#' onclick= 'javascript:SScalendar(\""+ssYear+"\", \""+(ssMonth-1)+"\")'><</a>";
-//calendar += ssToday.getFullYear() + '년' + (ssToday.getMonth()+1) + '월';
-//calendar += "<a href='#' onclick= 'javascript:SScalendar(\""+ssYear+"\", \""+(ssMonth+1)+"\")'>></>";
-//calendar += "</p>";
-//calendar += "<table class='calendar_table'>"
-//calendar += "<tr>";
-//calendar += "<th class='sun'>일</th>";
-//calendar += "<th>월</th>";
-//calendar += "<th>화</th>";
-//calendar += "<th>수</th>";
-//calendar += "<th>목</th>";
-//calendar += "<th>금</th>";
-//calendar += "<th class='sat'>토</th>";
-//calendar += "</tr>";
-//
-//var countDate = 1;
-//var count = 0;
-//var prevCountDate = lastDate[ssMonth-1] - (ssDay-1);
-//var nextCountDate = 1;
-//
-//for(var i=1; i<=week; i++) {
-//    calendar += "<tr>";
-//    for(var j=1; j<=7; j++) {
-//        if(i==1 && j<=ssDay) {
-//            calendar += "<td class='prev'>" + prevCountDate + "</td>";
-//            prevCountDate++;
-//        } else if(countDate>nowLastDate) {
-//            calendar += "<td class='next'>" + nextCountDate + "</td>";
-//            nextCountDate++;
-//        } else if(ssToday.getFullYear()==ssYear&&ssToday.getMonth()==ssMonth&&countDate==date) {
-//            calendar += "<td class='today'>" + countDate + "</td>";
-//            countDate++;
-//        } else if(count==0) {
-//            calendar += "<td class='sun'>" + countDate + "</td>";
-//            countDate++;
-//        } else if(count==6) {
-//            calendar += "<td class='sat'>" + countDate + "</td>";
-//            countDate++;
-//        } else {
-//            calendar += "<td>" + countDate + "</td>";
-//            countDate++;
-//        }
-//        count++;
-//        if(count==7) {
-//            count = 0;
-//        }
-//    }
-//    calendar += "</tr>";
-//}
-//
-//document.getElementById("SScalendar").innerHTML = calendar;
-//
-//}
-
 function SScalendar(id, today) {
 	
 if( typeof(today) != 'undefined' ) {
@@ -127,7 +39,7 @@ calendar += year + '년' + month + '월';
 calendar += "<a href=#' class='button' onclick= 'SScalendar(\""+id+"\", \""+nextMonthToday+"\")'><img src='btn_month_next.gif'/></a>";
 calendar += "<a href='#' class='button' onclick= 'SScalendar(\""+id+"\", \""+nextYearToday+"\")'><img src='btn_year_next.gif'/></a>";
 calendar += "</p>";
-calendar += "<table class='calendar_table'>"
+calendar += "<table class='calendar_table'>";
 calendar += "<tr>";
 calendar += "<th class='sun'>일</th>";
 calendar += "<th>월</th>";
@@ -149,7 +61,10 @@ var nextCountDate = 1;
 for(var i = 1; i <= week; i++) {
     calendar += "<tr>";
     for(var j = 1; j <= 7; j++) {
-        if(i == 1 && j <= day) {
+        if(year == d.getFullYear() && month == d.getMonth() + 1 && countDate == d.getDate()) {
+            calendar += "<td style='background-color: #cccccc'>" + countDate + "</td>";
+            countDate++;
+        } else if(i == 1 && j <= day) {
             calendar += "<td class='prev'>" + prevCountDate + "</td>";
             prevCountDate++;
         } else if(countDate > nowLastDate) {
@@ -160,9 +75,6 @@ for(var i = 1; i <= week; i++) {
             countDate++;
         } else if(count == 6) {
             calendar += "<td class='sat'>" + countDate + "</td>";
-            countDate++;
-        } else if(year == d.getFullYear() && month == d.getMonth() + 1 && countDate == d.getDate()) {
-            calendar += "<td style='background-color: #cccccc'>" + countDate + "</td>";
             countDate++;
         } else {
             calendar += "<td>" + countDate + "</td>";
